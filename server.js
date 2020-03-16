f2c91a0c09a5f12f0354c543f82abcd11dad3614
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-16 19:00:48 +1100

    Update server to remove the hardcoded url

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+dotenv.config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static(" client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
