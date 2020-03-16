d311d8b89ecc8e267a569c4feda12007c1080cfb
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-17 00:03:10 +1100

    Modify app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
+import BookContext from './utils/BookContext';
+import API from "./utils/API";
+
 function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
+	const [books, setBooks] = useState([]);
+
+	useEffect(()=> {
+		API.getBooks()
+			.then(({data}) => {
+				setBooks(data);
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}, []);
+
+	const saveBook = (book) => {
+		API
+			.saveBook(book)
+			.then(({data}) => {
+				setBooks([...books,data])
+			})
+			.catch(err => console.error(err));
+	}
+	const deleteBook = (bookId) => {
+		API
+			.deleteBook(bookId)
+			.then((_) => setBooks(books.filter((b)=> b.googleId !== bookId)) )
+			.catch(err => console.error(err));
+	}
+
+	const AppContent = {
+		books,
+		saveBook,
+		deleteBook
+	}
+
+	return (
+		<Router>
+		<div>
+			<Nav />
+			<BookContext.Provider value={AppContent}>
+				<Switch>
+					<Route exact path="/" component={Search} />
+					<Route exact path="/search" component={Search} />
+					<Route exact path="/saved" component={Saved} />
+					<Route component={NoMatch} />
+				</Switch>
+			</BookContext.Provider>
+		</div>
+		</Router>
+	);
 }
 
 export default App;
